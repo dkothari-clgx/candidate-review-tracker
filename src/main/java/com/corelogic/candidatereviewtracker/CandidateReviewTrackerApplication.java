101940517cb9d63d9d1bad67b751cbf9c2acf0ca
dkothari-clgx <dkothari@corelogic.com>
2021-02-24 21:36:45 -0800

    Initial Commit

---
+++ src/main/java/com/corelogic/candidatereviewtracker/CandidateReviewTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.corelogic.candidatereviewtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateReviewTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateReviewTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corelogic/candidatereviewtracker/CandidateReviewTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corelogic.candidatereviewtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateReviewTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
