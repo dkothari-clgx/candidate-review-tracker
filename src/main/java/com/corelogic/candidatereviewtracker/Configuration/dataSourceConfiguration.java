7e354e0830aaf5e42d532b290886a9e30585b3bf
Darshan Kothari <dkothari@corelogic.com>
2021-02-25 14:58:29 -0800

    Fix Auditing

--- src/main/java/com/corelogic/candidatereviewtracker/Configuration/dataSourceConfiguration.java
+++ src/main/java/com/corelogic/candidatereviewtracker/Configuration/dataSourceConfiguration.java
@@ -1,11 +1,12 @@
 package com.corelogic.candidatereviewtracker.Configuration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories
-@EnableTransactionManagement
+@EnableJpaAuditing
 public class dataSourceConfiguration {
 
 }
